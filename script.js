f9d6f426fd3bec0deec1c6ed9838b9c26609e4f3
ezgihendrickx <ezgihendrickx@gmail.com>
2020-08-13 23:34:30 +0200

    Add JS comments

--- script.js
+++ script.js
@@ -1,13 +1,12 @@
-let hereAPIKey = "ZT-3uBrLouekcar_GoPf-ujFLV8xoCFomG-ZSPbUbts";
-let addressDetails = `https://geocode.search.hereapi.com/v1/geocode?q=Ghent&apiKey=${hereAPIKey}`;
+let hereAPIKey = "HY42AIbJoZSVlGRz0Dn-eea-HjU-Kj1GWDLq3pp1GH4"; // heremaps for the location
 //https://developer.here.com/documentation/geocoding-search-api/dev_guide/topics/endpoint-geocode-brief.html
-let latitude = 50;
-let longitude = 50;
-const FORECAST = document.getElementsByClassName("component__forecast-box")[0];
+let latitude;
+let longitude;
+const FORECAST = document.getElementsByClassName("component__forecast-box")[0]; // <div class="component__forecast-box"></div> getting this from HTML
 
 function getLocation(city) {
   fetch(
-    `https://geocode.search.hereapi.com/v1/geocode?q=${city}&apiKey=${hereAPIKey}`
+    `https://geocode.search.hereapi.com/v1/geocode?q=${city}&apiKey=${hereAPIKey}` //getting the location of the city
   )
     .then((items) => {
       return items.json();
@@ -16,6 +15,7 @@ function getLocation(city) {
 }
 
 function calcLonLat(items) {
+  //check it out above link  line 2
   latitude = items.items[0].position.lat;
   longitude = items.items[0].position.lng;
   locationName = items.items[0].title;
@@ -29,13 +29,14 @@ const api = {
 //api.openweathermap.org/data/2.5/forecast?q={city name}&appid={your api key}
 const searchbox = document.querySelector(".search");
 let locationName = searchbox.value;
-searchbox.addEventListener("keypress", setQuery);
-window.onload = getResults(searchbox.value);
+searchbox.addEventListener("keypress", setQuery); //when you enter of this input this function starts
+//window.onload = getLocation(city); // when you reload the website this function starts
 
 function setQuery(evt) {
+  //if you press the enter
   if (evt.keyCode == 13) {
-    FORECAST.innerHTML = "";
-    getLocation(searchbox.value);
+    FORECAST.innerHTML = ""; //empty the html forecast table
+    getLocation(searchbox.value); // check line 7 for that
     // console.log(searchbox.value);
   }
 }
@@ -43,50 +44,51 @@ function setQuery(evt) {
 function getResults(location) {
   console.log(latitude);
   fetch(
-    `${api.base}onecall?lat=${latitude}&lon=${longitude}&exlude=hourly&units=metric&APPID=${api.key}`
+    `${api.base}onecall?lat=${latitude}&lon=${longitude}&exlude=hourly&units=metric&APPID=${api.key}` //getting the weather
   )
     .then((weather) => {
-      return weather.json();
+      return weather.json(); //from json file
     })
     .then(displayResults);
 }
 
 function displayResults(weather) {
+  //shows everything in HTML
   //   console.log(weather);
   let city = document.querySelector(".location .city");
-  //city.innerText = `${weather.name}, ${weather.sys.country}`;
+
   city.innerText = locationName;
 
   let now = new Date();
   console.log(now);
   let date = document.querySelector(".location .date");
-  date.innerText = dateBuilder(now);
+  date.innerText = dateBuilder(now); //show the date
 
   let temp = document.querySelector(".current .temp");
   console.log(temp);
-  temp.innerHTML = `${Math.round(weather.current.temp)}<span>°c</span>`;
+  temp.innerHTML = `${Math.round(weather.current.temp)}<span>°c</span>`; //get the temp from json
 
   let weather_el = document.querySelector(".current .weather");
-  weather_el.innerText = weather.current.weather[0].main;
-  let hilow = document.querySelector(".hi-low");
-
+  weather_el.innerText = weather.current.weather[0].main; //weather description
+  let hilow = document.querySelector(".hi-low"); //feels like?
   hilow.innerText =
     "feels like " + Math.round(weather.current.feels_like) + "°c";
 
   weather.daily.forEach((day) => {
+    //change this later 5 days, this loop gives me 1 week weatherforecast
     console.log(day);
-    let date = new Date(day.dt * 1000);
+    let date = new Date(day.dt * 1000); //to get actual date from it you need to calc
     let days = ["Sun", "Mon", "Tues", "Wed", "Thurs", "Fri", "Sat"];
     let name = days[date.getDay()];
     let dayBlock = document.createElement("div");
-    dayBlock.className = "forecast__item";
+    dayBlock.className = "forecast__item"; //for icons temp actions
     dayBlock.innerHTML = `<div class="forecast-item__heading">${name}</div>
         <div class="forecast-item__info"><i class="wi ${applyIcon(
           day.weather[0].icon
         )}"></i> <span class="degrees">${Math.round(
       day.temp.day
     )}<i class="wi wi-degrees"></i></span></div>`;
-    FORECAST.appendChild(dayBlock);
+    FORECAST.appendChild(dayBlock); //puttin in the HTML
   });
 }
 
@@ -123,6 +125,7 @@ function dateBuilder(d) {
 }
 
 function applyIcon(icon) {
+  //choose the correct icon
   let selectedIcon;
   switch (icon) {
     case "01d":
